20729b9bf37092e32ea8e8cd23dc0783e5077f48
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-02-02 09:30:30 -0800

    Evaluate can print off numbers and Fizz when divisible by 3
    Divisible by 3 checks to see if a number is divisible by 3 and returns
    true or false

---
+++ FizzBuzzKata/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzzKata
+{
+	public class FizzBuzz
+	{
+		public static void Main (string[] args)
+		{
+			
+		}
+		public static bool DivisibleByThree(int Number)
+		{
+			return Number % 3 == 0;
+		}
+
+		public static void Evaluate(int Number)
+		{
+			for (int i = 1; i <= Number; i++)
+			{
+				if (DivisibleByThree (i))
+				{
+					Console.WriteLine ("Fizz");
+				}
+				else
+				{
+					Console.WriteLine (i);
+				}
+			}
+		}
+	}
+}
---
+++ FizzBuzzKataTests/Test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Diagnostics;
+using FizzBuzzKata;
+
+namespace FizzBuzzKataTests
+{
+	[TestFixture]
+	public class FizzBuzzKataTests
+	{
+		[Test]
+		public void CanDivideByThree ()
+		{
+			int Number = 3;
+			bool Result = FizzBuzz.DivisibleByThree (Number);
+			Assert.IsTrue (Result, "Should return treu when it can divide by three");
+			int Number2 = 5;
+			bool Result2 = FizzBuzz.DivisibleByThree (Number2);
+			Assert.IsFalse (Result2, "Should return false when can't divide by three");
+		}
+
+		[Test]
+		public void PrintsFizz ()
+		{
+			int Number = 6;
+			Process p = new Process ();
+			p.StartInfo.FileName = "ipconfig";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.Start ();
+			string output = p.StandardOutput.ReadToEnd ();
+			p.WaitForExit ();
+			FizzBuzz.Evaluate (Number);
+			StringAssert.Contains (output, "1 2 Fizz 4 5 Fizz");
+		}
+	}
+}
+
