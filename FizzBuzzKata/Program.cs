5d1441e8abba8fa4cf32f0a68faaaca05e81fe5a
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2016-02-02 15:07:34 -0800

    Fizz buzz uses c# output by default using OutputStream method

--- FizzBuzzKata/Program.cs
+++ FizzBuzzKata/Program.cs
@@ -18,25 +18,31 @@ namespace FizzBuzzKata
 			return Number % 5 == 0;
 		}
 
-		public static void Evaluate(int Number)
+		public static void OutputStream(string message)
 		{
-			for (int i = 1; i <= Number; i++)
+			Console.WriteLine(message);
+		}
+
+
+		public static void Evaluate(int number)
+		{
+			for (int i = 1; i <= number; i++)
 			{
 				if (DivisibleByThree(i) & DivisibleByFive(i))
 				{
-					Console.WriteLine("FizzBuzz");
+					OutputStream("FizzBuzz");
 				}
 				else if (DivisibleByThree(i))
 				{
-					Console.WriteLine ("Fizz");
+					OutputStream("Fizz");
 				}
 				else if (DivisibleByFive(i))
 				{
-					Console.WriteLine("Buzz");
+					OutputStream("Buzz");
 				}
 				else
 				{
-					Console.WriteLine(i);
+					OutputStream(i.ToString());
 				}
 			}
 		}
